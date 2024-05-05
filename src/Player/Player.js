8d4e23dca5ca1fb3fd350688c1f1cd215dd95904
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2024-05-05 15:51:41 +0530

    wawa sensei controls

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { OrbitControls, Plane, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, Plane, PerspectiveCamera, KeyboardControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Guy from "./Guy";
 import Woman from "./Woman";
 import { Floor, Light_Common, Platform } from "../Game/Components/Floor";
 import { Physics, RigidBody } from "@react-three/rapier";
+import { useMemo } from "react";
 
 function Box() {
     const size = 0.3;
@@ -16,24 +17,41 @@ function Box() {
     );
 }
 
+export const Controls = {
+    forward: "forward",
+    back: "back",
+    left: "left",
+    right: "right",
+    jump: "jump",
+};
+
+
+
 function Player() {
+    const map = useMemo(
+        () => [
+            { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+            { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+            { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+            { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+            { name: Controls.jump, keys: ["Space"] },
+        ],
+        []
+    );
     return (
         <>
-            <Canvas style={{ height: "100vh" }} camera={{ position: [3, 4.5, 7.5], fov: 50 }} shadows>
-                <Physics>
-                    <OrbitControls />
-                    {/* our Guy is a Rigid Body */}
-                        {/* <group position={[0, 3, 0]}> */}
-                            <Guy />
-                        {/* </group> */}
-                    {/* the Floor is a fixed rigid body i.e does not move */}
-                    <RigidBody type="fixed">
-                        {/* <Platform color="#0D5D56" size={[30, "x", 30]} position={[0, -1.25, 0]} /> */}
-                        <Platform color="#0D5D56" size={[5, "x", 5]} position={[2, -1.25, 0]} />
-                    </RigidBody>
-                    <directionalLight castShadow position={[-1, 1, 1]} intensity={5} />
-                </Physics>
-            </Canvas>
+            <KeyboardControls map={map}>
+                <Canvas style={{ height: "100vh" }} camera={{ position: [3, 4.5, 7.5], fov: 50 }} shadows>
+                    <Physics debug>
+                        <OrbitControls />
+                        <Guy />
+                        <RigidBody type="fixed" name="floor">
+                            <Platform color="#0D5D56" size={[5, "x", 5]} position={[2, -1.25, 0]} />
+                        </RigidBody>
+                        <directionalLight castShadow position={[-1, 1, 1]} intensity={5} />
+                    </Physics>
+                </Canvas>
+            </KeyboardControls>
         </>
     );
 }
